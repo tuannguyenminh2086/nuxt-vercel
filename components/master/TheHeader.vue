cb45bedd90e44facb2857b13d6662bf52c373791
Tuan <minhtuan2086@gmail.com>
2022-05-07 14:12:04 +0700

    feat: add darkmode

--- components/master/TheHeader.vue
+++ components/master/TheHeader.vue
@@ -1,14 +1,19 @@
 <template>
   <header
-    class="sticky top-0 bg-white border-b border-slate-200 z-30 dark:bg-slate-900 dark:border-slate-500"
+    class="sticky shadow top-0 bg-white border-b border-slate-200 z-30 dark:bg-slate-900 dark:border-slate-500"
   >
-    <div class="px-4 sm:px-6 lg:px-8">
+    <div class="px-4 sm:px-6">
       <div class="flex items-center justify-between h-16 -mb-px">
-        <div class="flex">adasd</div>
-        <div class="flex">adasd</div>
+        <div class="flex">
+          <base-clock-item />
+        </div>
+        <div class="flex">
+          <base-theme-switcher />
+        </div>
       </div>
     </div>
   </header>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+</script>
