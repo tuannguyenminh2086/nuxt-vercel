b6bdc36d000ed6b39496c4b393210ce3dde653c7
Tuan <minhtuan2086@gmail.com>
2022-05-16 13:24:46 +0700

    feat: update code

---
+++ pages/tasks/create.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <base-section title="New Task">
+        <template #default>
+          <div class="p-4">
+            Create a component inside /components/tasks
+          </div>
+          
+     </template>
+    </base-section>
+  </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
