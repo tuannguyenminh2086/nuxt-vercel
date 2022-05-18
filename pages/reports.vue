226a607c9317c6446eb1fe6e497d47d9405aaa52
Hoang Nguyen <hoangnguyen@vietswiss.com>
2022-05-18 09:50:36 +0700

    feat: finish migrate report

--- pages/reports.vue
+++ pages/reports.vue
@@ -17,7 +17,7 @@
       </header>
       <div class='p-6'>
         <div class='mb-6 last:mb-0'>
-          <label class='block font-bold mb-2'>Grouped with icons</label>
+          <label class='block font-bold mb-2'>Spent time Report </label>
           <div class='grid grid-cols-1 gap-3 md:grid-cols-2'>
             <Datepicker v-model='state.date'
                         range
@@ -28,43 +28,16 @@
                         placeholder='Report Range'
                         class='relative max-w-full focus:ring focus:outline-none border-gray-700 rounded w-full dark:placeholder-gray-400 border bg-white dark:bg-gray-800'
             ></Datepicker>
-            <!--            <div class='relative'><input type='text'-->
-            <!--                                         class='px-3 py-2 max-w-full focus:ring focus:outline-none border-gray-700 rounded w-full dark:placeholder-gray-400 h-12 border bg-white dark:bg-gray-800 pl-10'-->
-            <!--                                         style='background-image: url(&quot;data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAYAAABzenr0AAAAAXNSR0IArs4c6QAAAfBJREFUWAntVk1OwkAUZkoDKza4Utm61iP0AqyIDXahN2BjwiHYGU+gizap4QDuegWN7lyCbMSlCQjU7yO0TOlAi6GwgJc0fT/fzPfmzet0crmD7HsFBAvQbrcrw+Gw5fu+AfOYvgylJ4TwCoVCs1ardYTruqfj8fgV5OUMSVVT93VdP9dAzpVvm5wJHZFbg2LQ2pEYOlZ/oiDvwNcsFoseY4PBwMCrhaeCJyKWZU37KOJcYdi27QdhcuuBIb073BvTNL8ln4NeeR6NRi/wxZKQcGurQs5oNhqLshzVTMBewW/LMU3TTNlO0ieTiStjYhUIyi6DAp0xbEdgTt+LE0aCKQw24U4llsCs4ZRJrYopB6RwqnpA1YQ5NGFZ1YQ41Z5S8IQQdP5laEBRJcD4Vj5DEsW2gE6s6g3d/YP/g+BDnT7GNi2qCjTwGd6riBzHaaCEd3Js01vwCPIbmWBRx1nwAN/1ov+/drgFWIlfKpVukyYihtgkXNp4mABK+1GtVr+SBhJDbBIubVw+Cd/TDgKO2DPiN3YUo6y/nDCNEIsqTKH1en2tcwA9FKEItyDi3aIh8Gl1sRrVnSDzNFDJT1bAy5xpOYGn5fP5JuL95ZjMIn1ya7j5dPGfv0A5eAnpZUY3n5jXcoec5J67D9q+VuAPM47D3XaSeL4AAAAASUVORK5CYII=&quot;); background-repeat: no-repeat; background-attachment: scroll; background-size: 16px 18px; background-position: 98% 50%; cursor: auto;'><span-->
-            <!--              class='inline-flex justify-center items-center w-10 h-12 absolute top-0 left-0 z-10 pointer-events-none text-gray-500 dark:text-gray-400'><svg-->
-            <!--              viewBox='0 0 24 24' width='16' height='16' class='inline-block'><path fill='currentColor'-->
-            <!--                                                                                    d='M12,4A4,4 0 0,1 16,8A4,4 0 0,1 12,12A4,4 0 0,1 8,8A4,4 0 0,1 12,4M12,14C16.42,14 20,15.79 20,18V20H4V18C4,15.79 7.58,14 12,14Z'></path></svg></span>-->
-            <!--            </div>-->
-            <!--            <div class='relative'><input type='email'-->
-            <!--                                         class='px-3 py-2 max-w-full focus:ring focus:outline-none border-gray-700 rounded w-full dark:placeholder-gray-400 h-12 border bg-white dark:bg-gray-800 pl-10'><span-->
-            <!--              class='inline-flex justify-center items-center w-10 h-12 absolute top-0 left-0 z-10 pointer-events-none text-gray-500 dark:text-gray-400'><svg-->
-            <!--              viewBox='0 0 24 24' width='16' height='16' class='inline-block'><path fill='currentColor'-->
-            <!--                                                                                    d='M20,4H4C2.89,4 2,4.89 2,6V18A2,2 0 0,0 4,20H20A2,2 0 0,0 22,18V6C22,4.89 21.1,4 20,4M17,17H7V15H17M17,13H7V11H17M20,9H17V6H20'></path></svg></span>-->
-            <!--            </div>-->
+            <button
+              class='w-1/3 inline-flex cursor-pointer justify-center items-center whitespace-nowrap focus:outline-none transition-colors focus:ring duration-150 border rounded ring-blue-700 bg-blue-600 text-white border-blue-700 hover:bg-blue-700 '
+              type='button'
+              @click.prevent='handleSubmit'
+            ><!----><span class='px-2'>Download</span>
+            </button>
           </div><!---->
 
         </div>
-        <div class='mb-6 last:mb-0'><label class='block font-bold mb-2'>With help line</label>
-          <div class=''>
-            <div class='relative'><input placeholder='Your phone number' type='tel'
-                                         class='px-3 py-2 max-w-full focus:ring focus:outline-none border-gray-700 rounded w-full dark:placeholder-gray-400 h-12 border bg-white dark:bg-gray-800'>
-              <!----></div>
-          </div>
-          <div class='text-xs text-gray-500 dark:text-gray-400 mt-1'>Do not enter the leading zero</div>
-        </div>
-        <hr class='border-gray-100 my-6 -mx-6 dark:border-gray-800 border-t'>
 
-        <div class='flex items-center justify-start flex-wrap -mb-3'>
-          <button
-            class='inline-flex cursor-pointer justify-center items-center whitespace-nowrap focus:outline-none transition-colors focus:ring duration-150 border rounded ring-blue-700 p-2 bg-blue-600 text-white border-blue-700 hover:bg-blue-700 mr-3 last:mr-0 mb-3'
-            type='button'
-            @click.prevent='handleSubmit'
-          ><!----><span class='px-2'>Submit</span>
-          </button>
-          <button
-            class='inline-flex cursor-pointer justify-center items-center whitespace-nowrap focus:outline-none transition-colors focus:ring duration-150 border rounded ring-blue-700 p-2 text-blue-600 border-blue-700 hover:bg-blue-700 hover:text-white mr-3 last:mr-0 mb-3'
-            type='reset'><!----><span class='px-2'>Reset</span></button>
-        </div>
       </div>
     </form>
 
@@ -86,16 +59,15 @@ const _token = auth.getAuthToken()
 
 const handleSubmit = () => {
   let [startDate, endDate] = state.date
-
   startDate = transformDate(startDate)
   endDate = transformDate(endDate)
   submit(startDate, endDate)
 }
-const submit = async (startDate:string, endDate:string ) => {
+const submit = async (startDate: string, endDate: string) => {
   const runtimeConfig = useRuntimeConfig()
 
   const url = runtimeConfig.public.API_URL + '/reports/project-time-spent'
-  await useFetch(url, {
+  const { data, response } = await useFetch(url, {
     headers: {
       Authorization: `Bearer ${_token}`,
       Accept: 'application/json'
@@ -103,7 +75,21 @@ const submit = async (startDate:string, endDate:string ) => {
   }).post({
     startDate,
     endDate
-  }).json()
+  }).blob()
+
+  const responseHeader = response.value?.headers.get('Content-Disposition')
+  if (typeof responseHeader !== 'undefined' && responseHeader && data) {
+    const startFileNameIndex = responseHeader.indexOf('=') + 1
+    const filename = `${startDate}_${endDate}_` + responseHeader.substring(startFileNameIndex, responseHeader.length)
+    const reportData = data.value as Blob
+    const fileURL = window.URL.createObjectURL(reportData)
+    const fileLink = document.createElement('a')
+    fileLink.href = fileURL
+    fileLink.setAttribute('download', filename)
+    document.body.appendChild(fileLink)
+    fileLink.click()
+    state.date = ''
+  }
 
 }
 const transformDate = (date: string): string => {
