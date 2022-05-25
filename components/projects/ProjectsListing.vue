f56d70d8cb4edfc85b71e8496641b1627cccc5d7
Tuan <minhtuan2086@gmail.com>
2022-05-25 17:13:06 +0700

    feat: update code

--- components/projects/ProjectsListing.vue
+++ components/projects/ProjectsListing.vue
@@ -8,13 +8,13 @@
         <div v-else>
           <table class="table-auto border-collapse w-full">
               <thead>
-                <tr class="font-bold border-b bg-slate-50 dark:bg-slate-900 dark:border-0">
-                  <th class="py-3 px-4 font-bold text-left">ID</th>
-                  <th class="py-3 px-4 font-bold text-left">Project Name</th>
-                  <th class="py-3 px-4 font-bold text-left">Leader</th>
-                  <th class="py-3 px-4 font-bold text-left">Priority</th>
-                  <th class="py-3 px-4 font-bold text-left">Status</th>
-                  <th class="py-3 px-4 font-bold text-left">Tracked</th>
+               <tr class="border-slate-100 bg-slate-50 text-slate-400 text-sm uppercase dark:bg-slate-900 dark:border-0">
+                  <th class="py-3 px-4 font-bold text-sm text-left">#</th>
+                  <th class="py-3 px-4 font-bold text-sm text-left">Name</th>
+                  <th class="py-3 px-4 font-bold text-sm text-left">Leader</th>
+                  <th class="py-3 px-4 font-bold text-sm text-left">Priority</th>
+                  <th class="py-3 px-4 font-bold text-sm text-left">Status</th>
+                  <th class="py-3 px-4 font-bold text-sm text-left">Tracked</th>
                 </tr>
               </thead>
 
@@ -27,7 +27,7 @@
                 >
 
                   <td class="py-3 px-4">{{ project.id }}</td>
-                  <td class="py-3 px-4"><NuxtLink :to="`/projects/${project.id}`">{{ project.name }}</NuxtLink></td>
+                  <td class="py-3 px-4"><NuxtLink :to="`/projects/${project.id}`" class="text-blue-500 font-bold text-lg hover:text-blue-600">{{ project.name }}</NuxtLink></td>
                   <td class="py-3 px-4">{{ project.leader? project.leader.name : 'n/a' }}</td>
                   <td class="py-3 px-4 "><base-priority v-if="project.mapped_priority" :text="project.mapped_priority.toLowerCase()" /></td>
                   <td class="py-3 px-4 ">n/a</td>
--- pages/reports.vue
+++ pages/reports.vue
@@ -1,50 +1,47 @@
 <template>
   <NuxtLayout name='dashboard'>
-    <h1 class='text-4xl font-bold'>Reports</h1>
+    <h1 class='text-4xl font-bold mb-10'>Reports</h1>
 
-    <NuxtLink to='/'> Home</NuxtLink>
 
-    <form class='md:rounded border-gray-100 dark:bg-gray-900/70 bg-white border dark:border-gray-800' _lpchecked='1'>
-      <header class='border-gray-100 flex items-stretch border-b dark:border-gray-800'>
-        <p class='flex items-center py-3 grow font-bold px-4'>
-          <span
-            class='inline-flex justify-center items-center w-6 h-6 mr-3'>
-            <svg viewBox='0 0 24 24' width='16' height='16' class='inline-block'>
-              <path 
-                fill='currentColor'
-                d='M13,9.5H18V7.5H13V9.5M13,16.5H18V14.5H13V16.5M19,21H5A2,2 0 0,1 3,19V5A2,2 0 0,1 5,3H19A2,2 0 0,1 21,5V19A2,2 0 0,1 19,21M6,11H11V6H6V11M7,7H10V10H7V7M6,18H11V13H6V18M7,14H10V17H7V14Z'>
-              </path>
-            </svg>
-          </span>
-          Report
-        </p>
+    <base-section title="Time Tracking Report">
+      <template #default>
 
-      </header>
-      <div class='p-6'>
-        <div class='mb-6 last:mb-0'>
-          <label class='block font-bold mb-2'>Spent time Report </label>
-          <div class='grid grid-cols-1 gap-3 md:grid-cols-2'>
-            <Datepicker v-model='state.date'
-                        range
-                        format='MM/dd/yyyy'
-                        multi-calendars
-                        close-on-scroll
-                        auto-apply
-                        placeholder='Report Range'
-                        class='relative max-w-full focus:ring focus:outline-none border-gray-700 rounded w-full dark:placeholder-gray-400 border bg-white dark:bg-gray-800'
-            ></Datepicker>
-            <button
-              class='w-1/3 inline-flex cursor-pointer justify-center items-center whitespace-nowrap focus:outline-none transition-colors focus:ring duration-150 border rounded ring-blue-700 bg-blue-600 text-white border-blue-700 hover:bg-blue-700 '
-              type='button'
-              @click.prevent='handleSubmit'
-            ><!----><span class='px-2'>Download</span>
-            </button>
-          </div><!---->
 
-        </div>
+            <form class='dark:bg-gray-900/70 bg-white dark:border-gray-800'>
+     
+              <div class='p-6'>
+                <div class='mb-6 last:mb-0'>
 
-      </div>
-    </form>
+
+                  <div class='grid grid-cols-4'>
+                    <Datepicker
+                      v-model='state.date'
+                      range
+                      format='MM/dd/yyyy'
+                      close-on-scroll
+                      auto-apply
+                      placeholder='Report Range'
+                      class='relative max-w-full focus:ring focus:outline-none border-slate-100 rounded w-full dark:placeholder-gray-400 border bg-white dark:bg-gray-800'
+                    />
+
+                    <button
+                      class='ml-4 w-2/4  inline-flex cursor-pointer justify-center items-center whitespace-nowrap focus:outline-none transition-colors focus:ring duration-150 border rounded ring-blue-700 bg-blue-600 text-white border-blue-700 hover:bg-blue-700 '
+                      type='button'
+                      @click.prevent='handleSubmit'
+                    >
+                      <span class='px-6 text-lg'>Download CSV</span>
+                    </button>
+
+                  </div>
+
+                </div>
+
+              </div>
+            </form>
+            
+      </template>
+    </base-section>
+
 
   </NuxtLayout>
 </template>
