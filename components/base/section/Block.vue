c049c001b560ee06a0d7fb62aec02b38ee260180
Tuan <minhtuan2086@gmail.com>
2022-05-25 13:37:16 +0700

    feat: update code: fix error

--- components/base/section/Block.vue
+++ components/base/section/Block.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <h6 class="text-xs font-semibold text-slate-400 uppercase mb-2">{{ title }}</h6>
+    <h6 class="text-xs font-semibold rounded-lg text-slate-400 uppercase mb-2">{{ title }}</h6>
     <div>
       <slot name="content" />
     </div>
