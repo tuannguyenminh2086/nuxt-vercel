eda7b1502f29c99fd15d495173e980c96e6fb255
Tuan <minhtuan2086@gmail.com>
2022-06-01 11:10:10 +0700

    feat: update code

--- components/base/members/Members.vue
+++ components/base/members/Members.vue
@@ -1,16 +1,34 @@
 <template>
   <div v-if="members" class="flex flex-wrap">
+    <div v-if="!pivot">
+      <div
+        v-for="(member, index) in members"
+        :key="index"
+        class="flex flex-row items-center mr-2 mb-2"
+      >
+          <div class="border rounded-full p-1 w-8 bg-slate-200 flex items-center justify-center overflow-hidden mr-2">
+            <img :src="member.avatar ? member.avatar : randowAvatar " class="object-cover w-full" />
+          </div>
+          <span v-if="showName" class="text-sm font-semibold">{{ member.name ? member.name : 'n/a'}}</span>          
+      </div>
+    </div>
+    <div v-else>
+      <div
+        v-for="(member, index) in members"
+        :key="index"
+        class="flex flex-row items-center mr-2 mb-2"
+      >
+          <div
+            v-if="member.pivot.is_main"
+            class="border rounded-full p-1 w-8 bg-slate-200 flex items-center justify-center overflow-hidden mr-2">
+            <img :src="member.avatar ? member.avatar : randowAvatar " class="object-cover w-full" />
+          </div>
+          <span v-if="showName && member.pivot.is_main" class="text-sm font-semibold">{{ member.name ? member.name : 'n/a'}}</span>
 
-    <div
-      v-for="(member, index) in members"
-      :key="index"
-      class="flex flex-row items-center mr-2 mb-2"
-    >
-        <div class="border rounded-full p-1 w-8 bg-slate-200 flex items-center justify-center overflow-hidden mr-2">
-          <img :src="member.avatar ? member.avatar : randowAvatar " class="object-cover w-full" />
-        </div>
-        <span v-if="showName" class="text-sm font-semibold">{{ member.name ? member.name : 'n/a'}}</span>          
-     </div>
+      </div>
+    </div>
+
+     
   </div>
 </template>
 
@@ -20,6 +38,7 @@
     name: string
     username?: string,
     avatar: string
+    pivot: any
   }
 
   interface IMember {
@@ -29,7 +48,19 @@
   
   const props = defineProps<IMember>();
   const randowAvatar = computed(() => useRandomAvatar() )
-  
   const members = props.members
+
+  const pivot = computed(() => {
+     if ( props.members.length > 0) {
+        if ('pivot' in props.members[0]) {
+          return true
+        }
+     }
+
+     return false
+  })
+
+
+
   
 </script>
