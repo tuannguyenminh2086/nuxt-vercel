70d5a55299bdaed6aabd83178428781c36fd0330
Tuan <minhtuan2086@gmail.com>
2022-06-14 16:39:35 +0700

    feat: update project

--- components/base/welcome/Item.vue
+++ components/base/welcome/Item.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="welcome mb-10">
-    <h1 class="text-4xl font-bold capitalize lg:text-5xl">{{ welcomeText }}, 
-      <span class="text-4xl font-bold inline-block lg:text-5xl">{{ name }}</span>
+    <h1 class="text-2xl font-semibold capitalize lg:text-2xl">{{ welcomeText }}, 
+      <span class="text-4xl font-bold inline-block lg:text-4xl">{{ name }}</span>
     </h1>
-    <div class="subtitle is-6 text-lg font-semibold mt-3">Hope you have great day!</div>
+    <div class="subtitle is-6 text-lg mt-3">Hope you have great day!</div>
   </div>
 </template>
 
@@ -19,11 +19,11 @@
       const today = new Date();
       const curHr = today.getHours();
       if (curHr < 12) {
-        return "Good morning";
+        return "Morning";
       } else if (curHr < 18) {
-        return "Good afternoon";
+        return "Afternoon";
       } else {
-        return "Good evening";
+        return "Evening";
       }
     });
 
--- layouts/dashboard.vue
+++ layouts/dashboard.vue
@@ -26,31 +26,33 @@
 </template>
 
 <script setup lang="ts">
-const { $echoClient, $notification, $bus } = useNuxtApp();
+  const { $echoClient, $notification, $bus } = useNuxtApp();
+
+  const updateActive = (message:any) => {
+      const id = Date.now()
+
+      $notification({
+        id,
+        type: 'warning',
+        title: 'Activity Tracking',
+        text: message
+      });
+
+      setTimeout(() => {
+        $notification.close(id);
+      }, 2000)
+      
+      $bus.$emit('refetch-activity');          
+  }
 
   onMounted(() => {
       $echoClient.private("TaskInProcess").listen(".task-in-process", (_e:any) => {
           const { data: { action, message } } = _e;
-          const id = Date.now()
-
+        
           if ( action) {
             switch (action) {
               case "reload":
-              
-                $notification({
-                  id,
-                  type: 'warning',
-                  title: 'Activity Tracking',
-                  text: message
-                });
-
-                setTimeout(() => {
-                  $notification.close(id);
-                }, 2000)
-                
-
-                $bus.$emit('refetch-activity');
-                
+                 updateActive(message)
               break;
             }
           }
@@ -59,8 +61,6 @@ const { $echoClient, $notification, $bus } = useNuxtApp();
   })
 
 
-  onUnmounted(() => {
 
-  })
 
 </script>
