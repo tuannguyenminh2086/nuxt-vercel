db28e78365b3694298446642231c67682c74e3d7
Kevin Müller <kevin@kliqs.ch>
2022-06-16 09:51:28 +0200

    feat: add edit task page

---
+++ components/base/tasks/GoToEdit.vue
@@ -0,0 +1,28 @@
+<template>
+  <div v-if="id" class="">
+    <button
+      class="text-white ml-2 bg-amber-600 rounded px-4 py-2 hover:bg-amber-700"
+      @click.prevent="edit"
+    >
+      Edit
+    </button>
+  </div>
+</template>
+
+<script setup lang="ts">
+
+  interface IProps {
+    id: string
+  }
+
+  const props = defineProps<IProps>()
+
+ 
+
+  const edit = () => {
+    navigateTo('/tasks/' + props.id + '/edit');
+  }
+
+</script>
+
+
